6c98f5a4030f06ee1eef3c47ebd96ad8ca90ebda
Md Raziur Rahman <153851061+Raziur12@users.noreply.github.com>
2025-08-08 15:48:49 +0530

    access cross origin

--- src/interceptor/axiosInstance.js
+++ src/interceptor/axiosInstance.js
@@ -6,36 +6,39 @@ const axiosInstance = axios.create({
   baseURL: baseURL,
   headers: {
     "Content-Type": "application/json",
+    "Access-Control-Allow-Origin": "https://srminstitute-ui.vercel.app",
+    "Access-Control-Allow-Credentials": "true",
+    "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
   }
 });
 
 
-const redirectToLogin = () => {
-  // You may replace this with your own logic to redirect the user to the login page
-  window.location.href = '/login';
-};
-const email = "kritgyakumar92@gmail.com"
-const password = "tempuser@123"
-const loginData = {
-    "email": email,
-    "password": password
-}
+// const redirectToLogin = () => {
+//   // You may replace this with your own logic to redirect the user to the login page
+//   window.location.href = '/login';
+// };
+// const email = "kritgyakumar92@gmail.com"
+// const password = "tempuser@123"
+// const loginData = {
+//     "email": email,
+//     "password": password
+// }
 
-const getCourses = () => {
-  axiosInstance  
-    .post("login", loginData)
-    .then((res) => {
-        localStorage.setItem("access", res.data.data.access);
-        localStorage.setItem("refresh", res.data.data.refresh);
-    })
-}
+// const getCourses = () => {
+//   axiosInstance  
+//     .post("login", loginData)
+//     .then((res) => {
+//         localStorage.setItem("access", res.data.data.access);
+//         localStorage.setItem("refresh", res.data.data.refresh);
+//     })
+// }
 
-const token = localStorage.getItem("access");
-if(!token) {
-  getCourses()
-} else {
-  getCourses()
-}
+// const token = localStorage.getItem("access");
+// if(!token) {
+//   getCourses()
+// } else {
+//   getCourses()
+// }
 
 // Add an interceptor for setting the Authorization header with the access token
 axiosInstance.interceptors.request.use(
